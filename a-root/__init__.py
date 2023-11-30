49e8dc7917c34bb5730871e4b809bd4013163ae9
IkezawaYuki <y.ikezawa93@gmail.com>
2023-11-30 22:25:00 +0900

    mod

---
+++ a-root/__init__.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE={
+            'user': 'username',
+            'password': 'password',
+            'host': 'localhost',
+            'database': 'database_name'
+        },
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.update(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "こんにちは、にっぽん"
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
