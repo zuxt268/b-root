4071ee2ddf6c3742869ac46f1bf8defbccc3a874
IkezawaYuki <y.ikezawa93@gmail.com>
2023-12-12 23:06:08 +0900

    mod

--- a-root/client.py
+++ a-root/client.py
@@ -55,7 +55,7 @@ class Meta(Client):
     def get_post(self, access_token, id):
         params = dict()
         params["access_token"] = access_token
-        params['fields'] = "caption,media_url"
+        params['fields'] = "id,caption,media_url,timestamp,media_type"
         return self.get(path=f"/{id}", params=params)
 
     def get_posts(self, access_token):
--- a-root/task.py
+++ a-root/task.py
@@ -7,6 +7,27 @@ from .client import get_meta_client, Wordpress
 from flask import Template
 
 
+def is_target(post):
+    if post["media_type"] != "IMAGE":
+        return False
+    cursor = get_db().cursor(dictionary=True)
+    cursor.execute(
+        "select * from posts where media_id = %s", (post["id"],)
+    )
+    exist = cursor.fetchone()
+    cursor.close()
+    if exist:
+        return False
+    return True
+
+
+def save_target(post):
+    db = get_db()
+    db.cursor().execute(
+
+    )
+
+
 def execute():
     cursor = get_db().cursor(dictionary=True)
     cursor.execute(
@@ -22,14 +43,15 @@ def execute():
         files = []
         index = 0
         for post in posts:
+            if not is_target(post):
+                continue
             # pngの場合のも対応すること
-            print("==========start")
-            print(post)
-            print("==========end")
             urlretrieve(post["media_url"], f"a-root/image_files/{index}.jpeg")
             files.append({
+                "media_id": post["id"],
+                "timestamp": post["timestamp"],
                 "caption": post["caption"],
-                "file_path": f"a-root/image_files/{index}.jpeg" # pngの場合のも対応すること
+                "file_path": f"a-root/image_files/{index}.jpeg"
             })
             index += 1
 
@@ -41,6 +63,12 @@ def execute():
             <img src="{resp['source_url']}" />
             """
             resp = wordpress.post_with_image(post["caption"], Template(html).render(), resp["id"])
+            # wordpress投稿response start ========
+            print(resp)
+            # wordpress投稿response end  =========
+
+
+
         shutil.rmtree("a-root/image_files")
         os.mkdir("a-root/image_files")
 
