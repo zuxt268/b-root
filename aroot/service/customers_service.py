224f34344f1a72c394c2a6919765f7129a5dca8a
IkezawaYuki <y.ikezawa93@gmail.com>
2024-03-14 17:57:53 +0900

    mod

--- aroot/service/customers_service.py
+++ aroot/service/customers_service.py
@@ -32,8 +32,7 @@ class CustomersService:
         raise CustomerNotFoundError("Customer with email {} not found".format(email))
 
     def update_facebook_token(self, id_, access_token):
-        customer = self.customers_repository.find_by_id(id_)
-        self.customers_repository.update(customer, {"facebook_token": access_token})
+        self.customers_repository.update(id_, {"facebook_token": access_token})
 
     def find_all(self):
         return self.customers_repository.find_all()
