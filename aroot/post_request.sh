9b5d0a5167647058c060de35757120aa2f4dd056
IkezawaYuki <y.ikezawa93@gmail.com>
2024-05-14 18:42:24 +0900

    batch file

--- aroot/post_request.sh
+++ aroot/post_request.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -X POST https://sd-a-root.info/batch
+echo "$(date): Executing POST request." >> /var/www/b-root/aroot/batch.log
+curl -X POST https://sd-a-root.info/batch >> /var/www/b-root/aroot/batch.log 2>&1
